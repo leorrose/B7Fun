bf9a3b370e0ebae7648a676d4aa0f241816424d9
leorrose <leor.rose@gmail.com>
2020-04-04 18:46:21 +0300

    PM2020T17-10 UI start

--- B7FunDjango/B7FunDjango/settings.py
+++ B7FunDjango/B7FunDjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'feed',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'B7FunDjango.urls'
+ROOT_URLCONF = 'feed.urls'
 
 TEMPLATES = [
     {
