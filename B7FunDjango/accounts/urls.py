47fa83170490afff1e7affaf7252e4e6edf36560
oritabibi <oritabibi7778@gmail.com>
2020-04-04 14:47:07 +0300

    PM2020T17-1 signup fix

--- B7FunDjango/accounts/urls.py
+++ B7FunDjango/accounts/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup', views.signup_view, name="signup"),
-    path('login', views.login_view, name='login'),
     path('', views.login_view, name='login'),
+    path('login', views.login_view, name='login'),
+    path('signup', views.signup_view, name="signup"),
     path('logout',views.logout_view,name='logout')
 
 ]
