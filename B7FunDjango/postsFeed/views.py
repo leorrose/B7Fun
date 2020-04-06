add1e68bd0bb2423c5cf48bb40aa4097fb247d80
Yahavba <yahavba356@gmail.com>
2020-04-06 17:37:48 +0300

    PM2020T17-212 unit tests

--- B7FunDjango/postsFeed/views.py
+++ B7FunDjango/postsFeed/views.py
@@ -4,7 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/')
 def posts(request):
-    if request.user.is_authenticated:
-        print(True)
     posts = PostFeed.objects.all().order_by('date')
     return render(request, 'postsFeed/posts.html', {'posts': posts})
