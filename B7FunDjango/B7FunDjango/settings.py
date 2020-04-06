2fa12dee22aee7d8231555a6a218194935b22f0e
oritabibi <oritabibi7778@gmail.com>
2020-04-06 15:53:16 +0300

    PM2020T17-23 retrieve password

--- B7FunDjango/B7FunDjango/settings.py
+++ B7FunDjango/B7FunDjango/settings.py
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'b7funservice@gmail.com'
-EMAIL_HOST_PASSWORD = 'ProjectService1#'
+EMAIL_HOST_PASSWORD = 'xvfrjnorytatpgms'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
