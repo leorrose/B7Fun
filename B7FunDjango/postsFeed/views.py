990e174e2de484924bc2fa2893e22ba4ee83f87f
yahavba <yahavba356@gmail.com>
2020-04-06 12:53:37 +0300

    PM2020T17-212 views

--- B7FunDjango/postsFeed/views.py
+++ B7FunDjango/postsFeed/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import PostFeed
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/')
 def posts(request):
+    if request.user.is_authenticated:
+        print(True)
     posts = PostFeed.objects.all().order_by('date')
     return render(request, 'postsFeed/posts.html', {'posts': posts})
