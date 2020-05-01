556430f594d91e9497667b22042a32406efbdbfa
hadar9 <hadar0934@gmail.com>
2020-05-01 19:07:16 +0300

    PM2020T17-94

--- B7FunDjango/accounts/admin.py
+++ B7FunDjango/accounts/admin.py
@@ -3,8 +3,13 @@
 # pylint: disable=missing-class-docstring
 
 from django.contrib import admin
-from .models import User
 from django.contrib.auth.models import Group
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import User
+from django.core import mail
+from .forms import EmailForm
+
 
 admin.site.unregister(Group)
 @admin.register(User)
@@ -18,3 +23,26 @@ class UserAdmin(admin.ModelAdmin):
         }),
     )
     search_fields = ['first_name', 'last_name']
+
+    def send_mail(self, request, queryset):
+        if 'apply' in request.POST:
+            email_form = EmailForm(request.POST)
+            if email_form.is_valid():
+                connection = mail.get_connection()
+                connection.open()
+
+                for user in queryset:
+                    email1 = mail.EmailMessage(
+                        email_form.cleaned_data['subject'],
+                        email_form.cleaned_data['content'],
+                        'b7funservice@gmail.com',
+                        [user.email],
+                        connection=connection,
+                    )
+                    email1.send()
+                self.message_user(request, "Sent mail to {} users".format(queryset.count()))
+                return HttpResponseRedirect(request.get_full_path())
+        return render(request, 'accounts/admin/send_mail.html', context={'users':queryset, 'form':EmailForm()})
+
+    actions = ['send_mail']
+    send_mail.short_description = "Send Email"
