c848cb02d9b56543a4bb1b38a5991a56f9ebdfd6
hadar9 <hadar0934@gmail.com>
2020-05-03 12:52:28 +0300

    Metric 2 pylint fixes

--- B7FunDjango/reviews/models.py
+++ B7FunDjango/reviews/models.py
@@ -12,8 +12,7 @@ class Review(models.Model):
     review_content = models.TextField(max_length=500, verbose_name="review_content")
     sender_email = models.EmailField(max_length=60, verbose_name="sender_email")
     sender_user_name = models.CharField(max_length=30, verbose_name="sender_user_name")
-    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)],
-                                 verbose_name="rating")
+    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)], verbose_name="rating")
 
     class Meta:
         db_table = 'reviews'
@@ -22,4 +21,3 @@ class Review(models.Model):
         if len(self.review_content) > 50:
             return self.sender_email + ' - ' + self.review_content[:50] + '...'
         return self.sender_email + ' - ' + self.review_content
-
