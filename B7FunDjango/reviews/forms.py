6b63355f9a3d10106afd9defbc925e2a4e4805f3
oritabibi <oritabibi7778@gmail.com>
2020-05-23 19:31:44 +0300

    PM2020T17-80 star rating

--- B7FunDjango/reviews/forms.py
+++ B7FunDjango/reviews/forms.py
@@ -7,7 +7,7 @@ from django import forms
 
 class ReviewForm(forms.Form):
     review_content = forms.CharField(label='תגובה', widget=forms.Textarea(
-        attrs={'class': 'form-control'}), initial='כתוב תגובה')
+        attrs={'class': 'form-control', 'dir': 'rtl', 'placeholder': 'כתוב תגובה'}))
     rating = forms.IntegerField(label='דרג')
 
 
