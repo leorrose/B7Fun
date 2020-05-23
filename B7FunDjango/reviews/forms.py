f0ddf43ffea0f7dfbb1efa804196e0323f09909c
oritabibi <oritabibi7778@gmail.com>
2020-05-23 14:54:57 +0300

    PM2020T17-80 update view form and urls

---
+++ B7FunDjango/reviews/forms.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+from django import forms
+
+
+class ReviewForm(forms.Form):
+    review_content = forms.CharField(label='תגובה', widget=forms.Textarea(
+        attrs={'class': 'form-control'}), initial='כתוב תגובה')
+    rating = forms.IntegerField(label='דרג')
+
+
--- B7FunDjango/reviews/views.py
+++ B7FunDjango/reviews/views.py
@@ -1,5 +1,28 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
 # pylint: disable=missing-class-docstring
+# pylint: disable=unnecessary-lambda
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from .forms import ReviewForm
+from .models import Review
+
+
+@login_required(login_url='/')
+def review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = Review.objects.create(review_content=form.cleaned_data.get('review_content'),
+                                           sender_email=request.user.email,
+                                           sender_user_name=request.user.user_name,
+                                           rating=form.cleaned_data.get('rating'))
+            print(review.review_content)
+            review.save()
+            redirect('feed:feed')
+    else:
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {'form': form})
+
+
