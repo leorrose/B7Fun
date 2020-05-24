98652d4af01d0b6cc1eaf84b3775f0c1794eceb3
hadar <hadar0934@gmail.com>
2020-05-24 23:53:47 +0300

    PM2020T17-89 //create app

---
+++ B7FunDjango/reports/admin.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=unused-argument
+# pylint: disable=arguments-differ
+
+from django.contrib import admin
+from .models import reports
+# Register your models here.
+
+
+
+@admin.register(reports)
+
+
+class ReportsAdmin(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ("subject", "sender_email", "date")
+    fieldsets = (
+        (None, {
+            'fields': ('sender_email', 'subject', 'content', 'date')
+        }),
+    )
+    search_fields = ['user_name', 'subject']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
---
+++ B7FunDjango/reports/forms.py
@@ -0,0 +1,26 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+from django import forms
+
+ch = [
+             ('מרכזים קהילתיים', ' מרכזים קהילתיים'),
+             ('גינות כלבים', 'גינות כלבים'),
+             ('ועדונים חברתיים לקשיש', 'מועדונים חברתיים לקשיש'),
+             ('מתקני משחק','מתקני משחק'),
+             ('מתקני ספורט','מתקני ספורט'),
+             ('אתרי טבע עירוניים','אתרי טבע עירוניים'),
+             ('אחר','אחר')
+
+]
+
+class reportsForm(forms.Form):
+
+    subject = forms.CharField(label='נושא', required=True, widget=forms.Select(
+        choices=ch, attrs={'class': 'form-control', 'dir': 'rtl', 'placeholder': 'כתוב נושא'}))
+    content = forms.CharField(label='דיווח', required=True, widget=forms.Textarea(
+        attrs={'class': 'form-control', 'dir': 'rtl', 'placeholder': 'כתוב דיווח'}))
+
+
+
